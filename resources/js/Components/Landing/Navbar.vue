0808b2485dea9a6fb80a18c8addf3ad740d1bba7
Abdallah El bialy <143313689+abduallahelbialy@users.noreply.github.com>
2024-04-09 14:50:40 +0200

    nav bar

--- resources/js/Components/Landing/Navbar.vue
+++ resources/js/Components/Landing/Navbar.vue
@@ -1,18 +1,13 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
-import Logo from '@/Components/Logo.vue';
-import Cart from '@/Components/Cart.vue';
-import { onMounted, ref } from 'vue';
-const showMenu = ref(false);
+import { Head, Link } from "@inertiajs/vue3";
+import Logo from "@/Components/Logo.vue";
+import Cart from "@/Components/Cart.vue";
+import { onMounted, ref } from "vue";
+const isMenuOpen = ref(false);
 
-function toggleNavbar() {
-    showMenu.value = !showMenu.value;
-}
-
-onMounted(() => {
-    const button = document.querySelector('.toggle-button');
-    button.addEventListener('click', toggleNavbar);
-});
+const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+};
 
 defineProps({
     canLogin: {
@@ -40,46 +35,42 @@ defineProps({
         type: Boolean,
     },
 });
-
-
 </script>
 
 <template>
     <div class="flex justify-center pupl">
-        <p class="text-black font-semibold">شريط اعلانات : <sapn class="text-red-600 font-semibold">انتظرونا قريبا</sapn></p> </div>
-    <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-white mb-3">
-        <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-            <div class=" jjj w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-startz">
-
-                <section v-if="canLogin" class="flex">
-                    <Link v-if="$page.props.auth.user" :href="route('admin.dashboard')"
-                        class=" btn-main text-white font-bold py-3  px-4 rounded lg:block hidden h-100 text-center">
-                    Dashboard</Link>
-                    <template v-else>
-
-                        <Link
-                            class=" btn-main text-white font-bold py-3  px-4 rounded lg:block hidden h-100 text-center"
-                            :href="route('login')">
-                        تسجيل الدخول
-
-                        </Link>
-                    </template>
-                    <!-- <Link
-                    :href="route('login')"
-                    class="login navbar-brand btn btn-main text-white d-none d-lg-block"
-                    >تسجيل الدخول</Link
-                > -->
-                    <Cart class="w-20 flex cart d-inline-block" />
-                    <Logo class=" logo max-w-full lg:hidden block   " />
-
-                </section>
-                <div class="flex">
-
-
-
-
+        <p class="text-black font-semibold">
+            شريط اعلانات :
+            <sapn class="text-red-600 font-semibold">انتظرونا قريبا</sapn>
+        </p>
+    </div>
+    <nav class="bg-white p-2">
+        <div class="flex justify-between items-center">
+            <!-- Cart & Login Button -->
+            <div class="flex items-center">
+                <button
+                    class="bg-pink-600 text-white px-4 py-2 rounded-lg mr-4 phon"
+                >
+                    تسجيل دخول
+                </button>
+                <div class="relative">
+                    <button class="text-white">
+                        <Cart class="w-20 flex cart d-inline-block" />
+                    </button>
+                    <!-- Cart Badge -->
                 </div>
-                <div class="notification relative cursor-pointer">
+            </div>
+            <!-- Logo -->
+            <div class="flex items-center">
+                <div class="w-24">
+                    <img
+                        src="https://mzaodin.com/website/log-removebg-preview.png"
+                        alt=""
+                    />
+                </div>
+            </div>
+            <!-- Toggle Menu Button -->
+             <div class="notification relative cursor-pointer">
                     <svg class="not-img absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="24"
                         width="24" id="Bell-Notification--Streamline-Nova">
                         <!-- <desc>Bell Notification Streamline Icon: https://streamlinehq.com</desc> -->
@@ -88,114 +79,142 @@ defineProps({
                         <path d="M15 21a3 3 0 0 1 -6 0z" fill="#be290b" stroke-width="2"></path>
                     </svg>
                 </div>
+            <div class="flex md:hidden">
                 <button
-                    class="  toggle-button main-text-color cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                    type="button">
-                    <i class="fas fa-bars"></i>
+                    id="toggleButton"
+                    class="text-white focus:outline-none bg-gray-600"
+                    @click="toggleMenu"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
                 </button>
             </div>
+            <!-- Links -->
+            <div
+                :class="{
+                    hidden: !isMenuOpen,
+                    'md:flex': true,
+                    'space-x-4': true,
+                }"
+            >
+                <ul class="flex space-x-4 bas">
+                    <li class="main-text-color">
+                        <i
+                            class="fas fa-users me-2 text-sm leading-lg main-text-color opacity-75"
+                        />
 
-            <div v-bind:class="{ 'hidden': !showMenu, 'flex': showMenu }" class="lg:flex lg:flex-grow items-center">
-
-                <ul class="flex flex-col lg:flex-row list-none ml-auto items-center">
-            <Logo class=" logo max-w-20 m-auto lg:block hidden  " />
-
-                    <li class="nav-item">
-                        <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug main-text-color hover:opacity-75"
-                            href="#pablo">
-                            <i class="fas fa-users text-lg leading-lg main-text-color opacity-75" /><span
-                                class="ml-2">من
-                                نحن</span>
-                        </a>
+                        <a href="#" class="text-white">من نحن</a>
                     </li>
+                    <li  class="main-text-color">
+                        <i
+                            class="fas fa-question-circle me-2 text-sm leading-lg main-text-color opacity-75"
+                        />
 
-                    <li class="nav-item">
-                        <Link
-                            class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug main-text-color hover:opacity-75 "
-                            :href="route('home')">
-                        <i class="fas fa-question-circle text-lg leading-lg main-text-color opacity-75" /><span
-                            class="ml-2">الاسئلة
-                            الشائعة</span>
-                        </Link>
+                        <a href="#" class="text-white">الاسئلة الشائعة</a>
                     </li>
-                    <li class="nav-item">
-                        <Link
-                            class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug main-text-color hover:opacity-75"
-                            :href="route('auctions')">
-                        <i class="fas fa-gavel text-lg leading-lg main-text-color opacity-75" /><span
-                            class="ml-2">المزادات</span>
-                        </Link>
+                    <li  class="main-text-color">
+                        <i
+                            class="fas fa-gavel me-2 text-sm leading-lg main-text-color opacity-75"
+                        />
+
+                        <a href="#" class="text-white">المزادات</a>
                     </li>
+                    <li  class="main-text-color">
+                        <i
+                            class="fas fa-home me-2 text-sm leading-lg main-text-color opacity-75"
+                        />
 
-                    <li class="nav-item">
-                        <Link
-                            class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug main-text-color hover:opacity-75"
-                            :href="route('home')">
-                        <i class="fas fa-home text-lg leading-lg main-text-color opacity-75" /><span
-                            class="ml-2">الرئيسية</span>
-                        </Link>
+                        <a href="#" class="text-white">الرئيسية</a>
                     </li>
-                    
-                    <!-- <li class="nav-item">
-                        <Link v-if="$page.props.auth.user"
-                            class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug main-text-color hover:opacity-75"
-                            :href="route('settings')">
-                        <i class="fas fa-cog text-lg leading-lg main-text-color opacity-75" /><span
-                            class="ml-2">الاعدادت</span>
-                        </Link>
-                    </li> -->
-                    <li v-if="canLogin" class="nav-item block my-5 lg:hidden">
-                        <Link v-if="$page.props.auth.user" :href="route('admin.dashboard')"
-                            class=" btn-main text-white font-bold py-3  px-4 rounded  h-100 text-center">
-                        Dashboard</Link>
-                        <template v-else>
-
-                            <Link class=" btn-main text-white font-bold py-3  px-4 rounded  h-100 text-center"
-                                :href="route('login')">
-                            تسجيل الدخول
-
-                            </Link>
-                        </template>
-
-                    </li>
+                    <div class="flex justify-end">
+                        <button
+                            class="bg-pink-600 text-white px-2 py-0 rounded-lg mr-4 yy"
+                        >
+                            تسجيل دخول
+                        </button>
+                    </div>
                 </ul>
             </div>
         </div>
     </nav>
 </template>
 <style scoped>
-.pupl{
+.pupl {
+    display: none;
+}
+.yy {
     display: none;
 }
 .notification {
     display: none;
 }
+.bas li a {
+    color: #fc40b9 !important;
+    font-size: 20px;
+    font-weight: bold;
+}
 
-@media (max-width:477px) {
+@media (max-width: 477px) {
     .notification {
         display: block;
     }
-.pupl{
-    display: flex;
-    border: 2px solid black;
-    background-color: rgb(113, 246, 65);
-    padding: 5px;
-    border-radius: 4px;
-}
+    .phon {
+        display: none;
+    }
+    .bas {
+        flex-direction: column-reverse;
+        position: absolute;
+        left: 0px;
+        width: 100%;
+        margin: auto !important;
+        top: 140px;
+        align-items: end;
+        padding: 5px 15px;
+        border-radius: 12px;
+        background-color: #fff !important;
+        box-shadow: 0 0 10px #ddd;
+        font-size: 20px;
+        font-weight: bold;
+        line-height: 44px;
+    }
+    .bas li a {
+        color: #fc40b9 !important;
+    }
+    .bas li  i{
+        display: none;
+    }
+    .pupl {
+        display: flex;
+        border: 2px solid black;
+        background-color: rgb(113, 246, 65);
+        padding: 5px;
+        border-radius: 4px;
+    }
     .not-img {
-        left: 10px;
-        top: 6px;
+      left: 64px;
+    top: -42px
     }
 }
-@media (min-width:1024px) {
-    .jjj{
+/* @media (min-width: 1024px) {
+    .jjj {
         margin-bottom: -102px;
     }
 }
-@media (min-width:1280px) {
-    .jjj{
+@media (min-width: 1280px) {
+    .jjj {
         margin-bottom: 0px;
     }
-}
-
+} */
 </style>
---
+++ resources/js/Components/Newlogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div >
+          <img :src="imageUrl"  alt="Image Alt Text ">
+
+  </div>
+</template>
+
+<script>
+export default {
+props: ['imagePath'], // تحديد الخاصية كمسار للصورة
+    data() {
+      return {
+        imageUrl: 'https://mzaodin.com/website/log-removebg-preview.png', // استخدام المسار الممرر أو الافتراضي
+      };
+    },
+}
+</script>
+
+<style>
+
+</style>
